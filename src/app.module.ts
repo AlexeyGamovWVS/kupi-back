2322c5296d4e99aeac56915e75fbc57bb4dc5b89
Alexey Gamov <lexa2009xxxx@yandex.ru>
2024-07-12 11:53:04 +0300

    Сгенерируйте каркас API

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
+import { Offer } from './offers/entities/offer.entity';
+import { User } from './users/entities/user.entity';
+import { Wish } from './wishes/entities/wish.entity';
+import { Wishlist } from './wishlists/entities/wishlist.entity';
 
 @Module({
   imports: [
@@ -12,9 +20,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'student',
       password: 'student',
       database: 'nest_project',
-      entities: [],
+      entities: [Offer, User, Wish, Wishlist],
       synchronize: true,
     }),
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+    OffersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
